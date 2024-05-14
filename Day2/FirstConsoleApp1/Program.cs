0d99be24fe674961e1a635f8e66076e99bbc381d
mithonk <mitho9798@gmail.com>
2024-05-14 15:31:01 +0530

    14.05.2024

--- Day2/FirstConsoleApp1/Program.cs
+++ Day2/FirstConsoleApp1/Program.cs
@@ -2,4 +2,18 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("My name is Mithursan");
 Console.WriteLine("My age is 26");
+
+
+int x = 10;
+int y = 5;
+
+Console.WriteLine($"{x} {y}");
+Console.WriteLine("x" + x);
+Console.WriteLine("y" + y);
+Console.WriteLine($"x{x}");
+Console.WriteLine($"y{y}");
+Console.WriteLine(x+y);
+
 Console.ReadLine();
+
+
