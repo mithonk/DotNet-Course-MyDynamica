1314aa11cdc5b51df7cc7ebd6946a873a459243e
mithonk <mitho9798@gmail.com>
2024-05-21 15:52:25 +0530

    21.05.2024

---
+++ Day3/FirstWindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace FirstWindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnMsg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(217, 85);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(286, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnMsg
+            // 
+            this.btnMsg.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnMsg.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMsg.Location = new System.Drawing.Point(377, 130);
+            this.btnMsg.Name = "btnMsg";
+            this.btnMsg.Size = new System.Drawing.Size(126, 36);
+            this.btnMsg.TabIndex = 2;
+            this.btnMsg.Text = "Click Me";
+            this.btnMsg.UseVisualStyleBackColor = false;
+            this.btnMsg.Click += new System.EventHandler(this.btnMsg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(615, 250);
+            this.Controls.Add(this.btnMsg);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "First Desktop App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnMsg;
+    }
+}
+
---
+++ Day3/HomeWork1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace HomeWork1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(227, 57);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(285, 22);
+            this.fname.TabIndex = 1;
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.SystemColors.Info;
+            this.btn.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.Location = new System.Drawing.Point(397, 174);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(115, 47);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "Click Me";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name";
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(227, 117);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(285, 22);
+            this.lname.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(618, 274);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "HW1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lname;
+    }
+}
+
