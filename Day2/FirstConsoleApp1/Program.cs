12d9670cd55b6bcd8b2debaf3a967a8d687a7b5b
mithonk <mitho9798@gmail.com>
2024-05-21 14:18:45 +0530

    21.05.2024

---
+++ Day2/FirstConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Mithursan Kalanantharasan");
+Console.WriteLine("My age is 26 years old");
+
+int x  = 10;
+int y = 10;
+
+Console.WriteLine("x : "+x);
+Console.WriteLine("y : "+y);
+Console.WriteLine($"x{x}");
+Console.WriteLine($"y{y}");
+Console.WriteLine($"{x} {y}");
+Console.WriteLine(x + y);
