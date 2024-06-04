f4a62f681acb9ece46cefb1cdedb6a3fcb356772
mithonk <mitho9798@gmail.com>
2024-06-04 09:36:03 +0530

    04-06-2024

---
+++ Day13-31-05-2024/WinFormsApp1/WinFormsApp1/DBconnection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace WinFormsApp1
+{
+    public partial class DBconnection : Form
+    {
+        public DBconnection()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            connetionString = "Server=MITHONK\\SQLEXPRESS;Database=CsarpDb;Trusted_Connection=True";
+
+            SqlConnection cnn;
+            cnn = new SqlConnection(connetionString);
+            
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+    }
+}
