5c0c208172e183175746e833da6b50ef30dc394f
mithonk <mitho9798@gmail.com>
2024-06-05 09:28:51 +0530

    05-06-2024

---
+++ Day14-04-06-2024/WinFormsApp1/WinFormsApp1/DBconnection.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsApp1
+{
+    partial class DBconnection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnTest = new Button();
+            SuspendLayout();
+            // 
+            // btnTest
+            // 
+            btnTest.Location = new Point(84, 63);
+            btnTest.Name = "btnTest";
+            btnTest.Size = new Size(142, 29);
+            btnTest.TabIndex = 0;
+            btnTest.Text = "Test Connection";
+            btnTest.UseVisualStyleBackColor = true;
+            btnTest.Click += btnTest_Click;
+            // 
+            // DBconnection
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(337, 172);
+            Controls.Add(btnTest);
+            Name = "DBconnection";
+            Text = "DBconnection";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnTest;
+    }
+}
