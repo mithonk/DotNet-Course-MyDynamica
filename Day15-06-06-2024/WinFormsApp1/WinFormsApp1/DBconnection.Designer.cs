1c2183c26f0cf1dd30428268024b784a28ec89f5
mithonk <mitho9798@gmail.com>
2024-06-06 15:49:21 +0530

    06-06-2024

---
+++ Day15-06-06-2024/WinFormsApp1/WinFormsApp1/DBconnection.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WinFormsApp1
+{
+    partial class DBconnection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnTest = new Button();
+            insertData = new Button();
+            viewData = new Button();
+            dataGridView1 = new DataGridView();
+            deleteData = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnTest
+            // 
+            btnTest.Location = new Point(12, 12);
+            btnTest.Name = "btnTest";
+            btnTest.Size = new Size(142, 29);
+            btnTest.TabIndex = 0;
+            btnTest.Text = "Test Connection";
+            btnTest.UseVisualStyleBackColor = true;
+            btnTest.Click += btnTest_Click;
+            // 
+            // insertData
+            // 
+            insertData.Location = new Point(160, 12);
+            insertData.Name = "insertData";
+            insertData.Size = new Size(142, 29);
+            insertData.TabIndex = 0;
+            insertData.Text = "Create Data";
+            insertData.UseVisualStyleBackColor = true;
+            insertData.Click += insertData_Click;
+            // 
+            // viewData
+            // 
+            viewData.Location = new Point(308, 12);
+            viewData.Name = "viewData";
+            viewData.Size = new Size(142, 29);
+            viewData.TabIndex = 0;
+            viewData.Text = "View Data";
+            viewData.UseVisualStyleBackColor = true;
+            viewData.Click += viewData_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 60);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(797, 370);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // deleteData
+            // 
+            deleteData.Location = new Point(456, 12);
+            deleteData.Name = "deleteData";
+            deleteData.Size = new Size(142, 29);
+            deleteData.TabIndex = 0;
+            deleteData.Text = "Delete Data";
+            deleteData.UseVisualStyleBackColor = true;
+            deleteData.Click += deleteData_Click;
+            // 
+            // DBconnection
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(821, 442);
+            Controls.Add(dataGridView1);
+            Controls.Add(deleteData);
+            Controls.Add(viewData);
+            Controls.Add(insertData);
+            Controls.Add(btnTest);
+            Name = "DBconnection";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "DBconnection";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnTest;
+        private Button insertData;
+        private Button viewData;
+        private DataGridView dataGridView1;
+        private Button deleteData;
+    }
+}
---
+++ Day15-06-06-2024/WinFormsApp1/WinFormsApp1/DBconnection.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace WinFormsApp1
+{
+    public partial class DBconnection : Form
+    {
+        int id;
+
+        public DBconnection()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+
+
+            connetionString = "Server=MITHONK\\SQLEXPRESS;Database=CsarpDb;Trusted_Connection=True; TrustServerCertificate=True;";
+
+            SqlConnection cnn;
+            cnn = new SqlConnection(connetionString);
+
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! " + ex.Message);
+            }
+        }
+
+        private void insertData_Click(object sender, EventArgs e)
+        {
+            /*
+            string connetionString = null;
+            string sql = null;
+
+            connetionString = "Server=MITHONK\\SQLEXPRESS;Database=CsarpDb;Trusted_Connection=True; TrustServerCertificate=True;";
+
+            SqlConnection cnn;
+            cnn = new SqlConnection(connetionString);
+
+            SqlCommand command;
+
+            sql = "INSERT INTO students (first_name, last_name, gender, grade, address) VALUES ('Mithursan', 'Kalanantharasan', 'Male', '10A', 'Jaffna')";
+
+            try
+            {
+                cnn.Open();
+                command = new SqlCommand(sql, cnn);
+                command.ExecuteNonQuery();
+                MessageBox.Show("Save Sucessfully");
+                cnn.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! " + ex.Message);
+            }
+            */
+
+            Form1 frm = new Form1();
+            frm.ShowDialog();
+        }
+
+        private void viewData_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            string sql = null;
+
+            connetionString = "Server=MITHONK\\SQLEXPRESS;Database=CsarpDb;Trusted_Connection=True; TrustServerCertificate=True;";
+
+            SqlConnection cnn;
+            cnn = new SqlConnection(connetionString);
+
+            SqlCommand command;
+
+            sql = "SELECT * FROM students";
+
+            try
+            {
+                cnn.Open();
+                command = new SqlCommand(sql, cnn);
+                SqlDataReader sqlReader = command.ExecuteReader();
+                /*
+                while (sqlReader.Read())
+                {
+                    MessageBox.Show(sqlReader.GetValue(0) + " - " + sqlReader.GetValue(1) + " - " + sqlReader.GetValue(2));
+                }
+                */
+
+                DataTable dt = new DataTable();
+                dt.Load(sqlReader);
+
+                dataGridView1.DataSource = dt;
+                sqlReader.Close();
+                command.Dispose();
+                cnn.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! " + ex.Message);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+        }
+
+        private void deleteData_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            string sql = null;
+
+            connetionString = "Server=MITHONK\\SQLEXPRESS;Database=CsarpDb;Trusted_Connection=True; TrustServerCertificate=True;";
+
+            SqlConnection cnn;
+            cnn = new SqlConnection(connetionString);
+
+            SqlCommand command;
+
+            try
+            {
+
+                if (id != 0)
+                {
+                    command = new SqlCommand("DELETE FROM students WHERE id=@id", cnn);
+                    cnn.Open();
+                    command.Parameters.AddWithValue("@id", id);
+                    command.ExecuteNonQuery();
+                    cnn.Close();
+                    MessageBox.Show("Record Deleted Successfully!");
+                   
+                }
+                else
+                {
+                    MessageBox.Show("Please Select the Record to DELETE");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+    }
+}
